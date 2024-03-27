42659b8f3ae1820b086245b0015df6a706f4b600
João Carvalho <joao.carvalho@maestrus.com>
2024-03-27 20:50:50 -0300

    work in progress

--- app_core/models.py
+++ app_core/models.py
@@ -5,9 +5,16 @@ from django.conf.global_settings import AUTH_USER_MODEL
 class UserOTPData(models.Model):
 
     user = models.ForeignKey(
-        AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name=''
+        AUTH_USER_MODEL, on_delete=models.CASCADE,
+        related_name='otpdata', null=False, blank=False,
+    )
+
+    two_factor_enabled = models.BooleanField(
+        null=False, blank=False, default=False,
+    )
+
+    otp_secret = models.CharField(
+        max_length=255, null=True, blank=True, default=''
     )
 
     class Meta:
---
+++ app_core/service.py
@@ -0,0 +1,39 @@
+
+class OTPService(object):
+
+    secret_key = ''
+
+    def __init__(self, secret_key):
+        self.secret_key = secret_key
+
+    def generate_auth_qrcode(self):
+        """
+        This method will generate the two-factor otp secret key for the
+        current user and the QR code image for the autentication App
+        :return: The SVG image for QR code painting
+        """
+        import pyotp
+        import qrcode.image.svg
+        self.secret_key = pyotp.random_base32()
+        totp = pyotp.TOTP(self.secret_key)
+        qr_uri = totp.provisioning_uri(
+            name='My fabulous application',
+            issuer_name='My fabulous application'
+        )
+        image_factory = qrcode.image.svg.SvgPathImage
+        qr_code_image = qrcode.make(
+            qr_uri,
+            image_factory=image_factory
+        )
+        return qr_code_image
+
+    def validate_otp(self, otp_code):
+        """
+        This method will validate the otp code for the current secret_key
+        :param otp_code:
+        :return: Boolean
+        """
+        import pyotp
+        totp = pyotp.TOTP(self.secret_key)
+        return totp.verify(otp_code)
+
